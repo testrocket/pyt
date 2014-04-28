054e02ec838e1db9c80b6ee251499ecdd50abc73
testrocket <rocketplusplus@gmail.com>
2014-04-28 23:54:39 +0200

    Custom search files extensions

--- get_emails/get_emails.py
+++ get_emails/get_emails.py
@@ -4,15 +4,15 @@ import csv
 import sys
 
 OUTPUT_FILENAME = "emails_set.csv"
-FILE_EXTENSIONS = ['.txt', '.csv']
-
 PATTERN_EMAIL = re.compile('([\w\-\.]+@(\w[\w\-]+\.)+[\w\-]+)')
+
+file_extensions = sys.argv[1:] or ['.txt', '.csv']
 
 emails = dict()
 def process(dirname, filename):
 	data = open(os.path.join(dirname, filename), 'rb')
 	for line in data:
-		for match in PATTERN_URL.findall(line):
+		for match in PATTERN_EMAIL.findall(line):
 			emails[match[0]] = dirname
 			
 def show_progress(current, total):
@@ -28,14 +28,18 @@ folder_counter = 0
 for dirname, dirnames, filenames in os.walk('.'):
 	folder_counter += 1
 	for filename in filenames:
-		for file_ext in FILE_EXTENSIONS:
+		for file_ext in file_extensions:
 			if filename.endswith(file_ext):
 				process(dirname, filename)
 				break
 
 	show_progress(folder_counter, total_folders)
 
-
+if not emails:
+	print 'No emails found'
+	exit(1)
+	
+print '\nFound %s emails' % (len(emails))
 emails_sorted = sorted(emails.keys())
 
 output_file = open(OUTPUT_FILENAME, 'wb')
