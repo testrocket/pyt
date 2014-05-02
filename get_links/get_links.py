23f8457ce40e21ab3aa45def11b7b02ff6b012d6
testrocket <rocketplusplus@gmail.com>
2014-05-03 01:52:10 +0200

    get links selenium script, first version

---
+++ get_links/get_links.py
@@ -0,0 +1,27 @@
+import re
+import sys
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
+driver.get("http://www.reddit.com")
+driver.maximize_window()
+
+site_table = driver.find_element(By.ID, "siteTable")
+entries = site_table.find_elements(By.CSS_SELECTOR, 'div[class*="thing id"]')
+
+links = []
+for entry in entries:
+	link = entry.find_element(By.CSS_SELECTOR, 'span.domain a')
+	if link.text == "i.imgur.com":
+		links.append(entry.find_element(By.CSS_SELECTOR, 'p.title a').get_attribute("href"))
+	
+for link in links:
+	body = driver.find_element(By.CSS_SELECTOR, 'body')
+	body.send_keys(Keys.CONTROL + 't')
+	body.send_keys(link)
+	body.send_keys(Keys.ENTER)
+
+	driver.implicitly_wait(2)
