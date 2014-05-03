9f96eca9b0001696dc1bf8705f89ea52b0f3f4ac
testrocket <rocketplusplus@gmail.com>
2014-05-03 02:24:27 +0200

    generator instead of list

--- get_links/get_links.py
+++ get_links/get_links.py
@@ -5,18 +5,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-min_points = 0
-if len(sys.argv) > 1:
-	pts = re.search("pts:\s*(\d+)", " ".join(sys.argv))
-	min_points = int(pts.group(1)) if pts else 0
-
 driver = webdriver.Firefox()
 driver.get("http://www.reddit.com")
 driver.maximize_window()
 
-def get_links(domain_name):
-	links = []
-
+def get_link(domain_name, min_points):
 	site_table = driver.find_element(By.ID, "siteTable")
 	entries = site_table.find_elements(By.CSS_SELECTOR, 'div[class*="thing id"]')
 	for entry in entries:
@@ -28,17 +21,18 @@ def get_links(domain_name):
 		
 		link = entry.find_element(By.CSS_SELECTOR, 'span.domain a')
 		if domain_name in link.text:
-			links.append(entry.find_element(By.CSS_SELECTOR, 'p.title a').get_attribute("href"))
+			yield entry.find_element(By.CSS_SELECTOR, 'p.title a').get_attribute("href")
 
-	return links
-	
-def open_links(links):
-	for link in links:
-		body = driver.find_element(By.CSS_SELECTOR, 'body')
-		body.send_keys(Keys.CONTROL + 't')
-		body.send_keys(link)
-		body.send_keys(Keys.ENTER)
+def new_tab(link):
+	body = driver.find_element(By.CSS_SELECTOR, 'body')
+	body.send_keys(Keys.CONTROL + 't')
+	body.send_keys(link)
+	body.send_keys(Keys.ENTER)
 
+min_points = 0
+if len(sys.argv) > 1:
+	pts = re.search("pts:\s*(\d+)", " ".join(sys.argv))
+	min_points = int(pts.group(1)) if pts else 0
 
-links = get_links("imgur.com")
-open_links(links)
+for link in get_link("imgur.com", min_points):
+	new_tab(link)
