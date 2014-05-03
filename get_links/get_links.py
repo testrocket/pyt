3283108f34eb3a65fa45bb9b3b8968cd30ccf62f
testrocket <rocketplusplus@gmail.com>
2014-05-03 02:21:21 +0200

    refactor

--- get_links/get_links.py
+++ get_links/get_links.py
@@ -14,23 +14,31 @@ driver = webdriver.Firefox()
 driver.get("http://www.reddit.com")
 driver.maximize_window()
 
-site_table = driver.find_element(By.ID, "siteTable")
-entries = site_table.find_elements(By.CSS_SELECTOR, 'div[class*="thing id"]')
+def get_links(domain_name):
+	links = []
 
-links = []
-for entry in entries:
-	score = entry.find_element(By.CSS_SELECTOR, 'div[class^="score unvoted"]')
-	points = int(score.text)
+	site_table = driver.find_element(By.ID, "siteTable")
+	entries = site_table.find_elements(By.CSS_SELECTOR, 'div[class*="thing id"]')
+	for entry in entries:
+		score = entry.find_element(By.CSS_SELECTOR, 'div[class^="score unvoted"]')
+		points = int(score.text)
+		
+		if points < min_points:
+			continue
+		
+		link = entry.find_element(By.CSS_SELECTOR, 'span.domain a')
+		if domain_name in link.text:
+			links.append(entry.find_element(By.CSS_SELECTOR, 'p.title a').get_attribute("href"))
+
+	return links
 	
-	if points < min_points:
-		continue
-	
-	link = entry.find_element(By.CSS_SELECTOR, 'span.domain a')
-	if "imgur.com" in link.text:
-		links.append(entry.find_element(By.CSS_SELECTOR, 'p.title a').get_attribute("href"))
-	
-for link in links:
-	body = driver.find_element(By.CSS_SELECTOR, 'body')
-	body.send_keys(Keys.CONTROL + 't')
-	body.send_keys(link)
-	body.send_keys(Keys.ENTER)
+def open_links(links):
+	for link in links:
+		body = driver.find_element(By.CSS_SELECTOR, 'body')
+		body.send_keys(Keys.CONTROL + 't')
+		body.send_keys(link)
+		body.send_keys(Keys.ENTER)
+
+
+links = get_links("imgur.com")
+open_links(links)
