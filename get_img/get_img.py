a9665ba085cad4f6a1b34cadd2d2c12dcbf2c50b
testrocket <rocketplusplus@gmail.com>
2014-05-04 19:30:33 +0200

    cleanup

--- get_img/get_img.py
+++ get_img/get_img.py
@@ -3,16 +3,21 @@ import sys
 
 from urllib import request
 
-if len(sys.argv) < 2:
-	print("Incorrect number of args")
+def error_msg(msg):
+	print(msg)
 	exit(1)
+
+def process(img_url, download=None):
+	print("number of bytes = %s " %len(request.urlopen(img_url).read()))
+
+if len(sys.argv) < 2:
+	error_msg("Incorrect number of args")
 
 cmdline_args = " ".join(sys.argv)
 
 url_match = re.search("-url\s+(\w+.\w+)", cmdline_args)
 if not url_match:
-	print("Url address not specified")
-	exit(1)
+	error_msg("Url address not specified")
 	
 url = "http://%s" % url_match.group(1)
 
@@ -22,21 +27,17 @@ connection.close()
 
 img_matches = re.findall('<img src="(.*?)"', html_content)
 if not img_matches:
-	print("Website %s does not contain any img tags" % url)
-	exit(1)
+	error_msg("Website %s does not contain any img tags" % url)
 
-contains_match = re.search("-contains\s+(\w+.*\w*) -*", cmdline_args)
+contains_match = re.search("-contains\s+(\w+.*\w*)\s*-*", cmdline_args)
 download_match = re.search("-download", cmdline_args)
 if not contains_match:
 	for img_match in img_matches:
 		print(img_match)
 	exit(0)
 
-def process(img_url, download=None):
-	print("number of bytes = %s " %len(request.urlopen(img_url).read()))
-
 contains_pattern = contains_match.group(1)
-print("sarch for contains pattern = %s" % contains_pattern)
+print(contains_pattern)
 for img_match in img_matches:
 	if contains_pattern in img_match:
 		process(img_match, download_match)
