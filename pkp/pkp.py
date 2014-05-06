a0c95232ff715e3d64469422ed84565fde8e7aba
testrocket <rocketplusplus@gmail.com>
2014-05-07 00:03:18 +0200

    unused import

--- pkp/pkp.py
+++ pkp/pkp.py
@@ -5,7 +5,6 @@ import re
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 
 if len(sys.argv) < 3:
 	print("Expected at least 2 arguments - start and stop station")
